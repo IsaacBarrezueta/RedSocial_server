6a475bfc6f1fbf5a84115dff4dd92871d4b5e102
IsaacBarrezueta <caisaacbare@gmail.com>
2024-02-25 23:29:09 -0500

    Cambio de BD a AVN

--- Enlace a Datos/database.js
+++ Enlace a Datos/database.js
@@ -1,11 +1,13 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',
-    host: 'localhost',
+    host: 'pg-320656b6-caisaacbare-3270.a.aivencloud.com',
+    port: '18664',
+    user: 'avnadmin',
+    password: 'AVNS_Idm0m4CmxDBhezLtrfF',
     database: 'RedSocial',
-    password: '1',
-    port: 5432
+    sslmode: 'require',
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
